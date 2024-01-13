71c9567de54543702a6a4a85d00fbf9614269463
akshay ashok <akshayashokan1054@gmail.com>
2024-01-13 23:01:57 +0530

    network layer added

--- H4X0R News/ContentView.swift
+++ H4X0R News/ContentView.swift
@@ -20,18 +20,6 @@ struct ContentView: View {
     }
 }
 
-struct Post: Identifiable{
-    let id : String
-    let title: String
-}
-
-
-let posts = [
-Post(id: "1", title: "Hello"),
-Post(id: "2", title: "Bonjour"),
-Post(id: "3", title: "Hola")
-]
-
 
 #Preview {
     ContentView()
