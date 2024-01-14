dff8e3f7d35a9da872e031942a6700454501bfed
akshay ashok <akshayashokan1054@gmail.com>
2024-01-15 01:35:51 +0530

    WebView added

--- H4X0R News/Views/ContentView.swift
+++ H4X0R News/Views/ContentView.swift
@@ -9,24 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     
-   @ObservedObject var networkManager = NetworkManager()
+    @ObservedObject var networkManager = NetworkManager()
     
     var body: some View {
         NavigationView{
             List(networkManager.posts){ post in
-                
-                HStack{
-                    Text(String(post.points))
-                    Text(post.title)
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack{
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
                 }
-               
+                
+                
             }.navigationTitle("H4X0R NEWS")
-        
+            
         }
         .onAppear(perform: {
             networkManager.fetchData()
         })
-    
+        
     }
 }
 
